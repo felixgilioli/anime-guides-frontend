8a093962ac9ccb6aab734f64d37430ac473d6097
Felix Gilioli <felix_gilioli@hotmail.com>
2021-07-06 17:58:17 -0300

    icon and title

--- src/components/AnimeCard.js
+++ src/components/AnimeCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -22,21 +21,19 @@ export default function AnimeCard() {
 
   return (
     <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          className={classes.media}
-          image="https://dg31sz3gwrwan.cloudfront.net/fanart/79824/970356-0-q80.jpg"
-          title="Naruto Shippuden Image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            Naruto Shippuden
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-          Naruto Shippuden is the continuation of the first part of the Naruto manga, and continues the same story line after spending two and a half years in the Naruto universe.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
+      <CardMedia
+        className={classes.media}
+        image="https://dg31sz3gwrwan.cloudfront.net/fanart/79824/970356-0-q80.jpg"
+        title="Naruto Shippuden Image"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          Naruto Shippuden
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+        Naruto Shippuden is the continuation of the first part of the Naruto manga, and continues the same story line after spending two and a half years in the Naruto universe.
+        </Typography>
+      </CardContent>
       <CardActions>
         <Button size="small" color="primary">
           Summary
